78934dc3cde797d1231f5fa0c6fb28312e1a7ab2
Ilan Silva Santos <ilansantos68@gmail.com>
2022-06-19 20:19:40 -0300

    Adicionado configuração de CORS

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const app = express();
-
+const cors = require("cors")
 // CONFIGS
 const PORT = 8000;
 dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
+app.use(cors());
 
 
 // ROUTES
