0ba5e1fccdbf3c5c747650fdfad4430093b868ef
Ilan Silva Santos <ilansantos68@gmail.com>
2022-06-19 20:20:13 -0300

    Adicionado filtro em consulta

--- routes/logistica/consulta.js
+++ routes/logistica/consulta.js
@@ -1,8 +1,16 @@
 const Medicamento = require("../../models/medicamento");
+const {Op} = require('sequelize');
 
 const Consulta = async (req, res) => {
     try{
-        const listaMedicamentos = await  Medicamento.findAll({raw: true});
+        console.log(req.query)
+        const listaMedicamentos = await Medicamento.findAll({raw: true, 
+            where: {
+                nome: {
+                    [Op.iLike]: req.query.nome ? req.query.nome + '%' : '%'
+                }
+            }
+        });
         res.status(200).json({mensagem: "Olá! você está na página de consulta.", dados: listaMedicamentos});
     }catch(erro){
         res.status(500).json({mensagem: "Houve um erro interno.", erro: erro.toString()})
